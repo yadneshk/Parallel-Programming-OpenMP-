1a4dd304c5ebb2d14f57f844c1db31e0c5ff3412
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-08-03 19:27:07 +0530

    Basic fucntions of OpenMP

---
+++ basic_funcs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <omp.h>
+int main()
+{
+     #pragma omp parallel
+     {
+          printf("The default number of threads= %d\n",omp_get_num_threads());
+          printf("Thread %d is executing now\n",omp_get_thread_num() );
+     }
+
+     /*Define specific number of threads to be used*/
+     #pragma omp parallel num_threads(2)
+     {
+          printf("The number threads running=%d\n",omp_get_num_threads() );
+     }
+
+     /*Maximum number of threads used for the current parallel region that does not use the clause num_threads.*/
+     printf("Maximum threads used for parallel region= %d\n",omp_get_max_threads() );
+
+     /*Number of threads used by default in subsequent parallel sections*/
+     omp_set_num_threads(3);
+     printf("Set number of threads to be used by default     =%d\n",omp_get_num_threads() );
+
+     #pragma omp parallel
+     {
+
+          printf("Thread\n");
+     }
+}
