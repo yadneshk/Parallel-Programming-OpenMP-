8b2dd0fea8a515fae9023271dbe3af985cfe3826
Yadnesh Kulkarni <yadnesh45@gmail.com>
2017-09-09 13:26:21 +0530

    calculates fibonaaci series for given number

--- exp5/fibonacci.c
+++ exp5/fibonacci.c
@@ -6,12 +6,17 @@ int main()
      int i=0,j=1,N,nextTerm;
      printf("Enter the limit\n");
      scanf("%d",&N);
-     for(i=1;i<=N;i++)
+     #pragma omp parallel
      {
-          printf("%d\t",i);
-          nextTerm=i+j;
-          i=j;
-          j=nextTerm;
+          #pragma omp for ordered
+          for(i=1;i<=N;i++)
+          {
+               #pragma omp ordered
+               printf("%d\t",i);
+               nextTerm=i+j;
+               i=j;
+               j=nextTerm;
+          }
      }
      printf("SUM=%d\n",nextTerm);
      return 0;
